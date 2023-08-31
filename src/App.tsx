6d71c0dfbb9f84a6af3ba9bd1689345e2fd00557
miskamero <miskamero@users.noreply.github.com>
2023-08-31 13:01:12 +0300

    Start template

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import './App.scss'
+
+const App = () => {
+  return (
+    <>
+    <div>
+    <header>
+      {/* <div className="logo">
+        <img src="https://i.imgur.com/8nKXQ8o.png" alt="logo" />
+      </div> */}
+      <nav>
+        <ul>
+          <li><a href="#">Log In</a></li>
+        </ul>
+      </nav>
+    </header>
+        <p>
+          This is a starter template for React, TypeScript, and Webpack. It
+          includes:
+        </p>
+        <ul>
+          <li>React</li>
+          <li>TypeScript</li>
+          <li>Webpack</li>
+          <li>SCSS</li>
+          <li>ESLint</li>
+          <li>Prettier</li>
+          <li>Stylelint</li>
+          <li>PostCSS</li>
+          <li>Autoprefixer</li>
+          <li>React Refresh</li>
+          <li>React Router</li>
+          <li>React Helmet</li>
+          <li>React Icons</li>
+        </ul>
+        <p>
+          <a href="https://github.com/miskamero/Group-project" target="_blank" rel="noopener noreferrer" >
+            GitHub Project Site
+            </a>
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App;
